3101b952cc730c2af40516a3df23222403c4c9f4
Arjan Jassal <arjan.jassal@poweredbygrow.com>
2018-09-15 16:13:58 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+export const test = (x) => {
+  if (Array.isArray(x)) {
+    console.log('hellooo');
+  }
+};
+
+export class What {
+  constructor(props) {
+    this.apples = props;
+  }
+
+  fuck() {
+    console.log('fuck');
+  }
+}
