27a1668b0919bff194d487f5c62d0e3bc20a7d6f
Arjan Jassal <arjan.jassal@poweredbygrow.com>
2018-09-15 18:25:32 -0700

    adding flow types and setting up rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,30 @@
 const resolve = require('rollup-plugin-node-resolve');
-const commonjs = require('rollup-plugin-commonjs');
+const babel = require('rollup-plugin-babel');
 
 export default [
-	// browser-friendly UMD build
-	{
-		input: 'src/index',
-		output: {
-      file: './lib/index.js',
-			format: 'cjs',
-		},
-		plugins: [
-			resolve(), // so Rollup can find `ms`
-			commonjs() // so Rollup can convert `ms` to an ES module
-		]
-	},
-
-	// // CommonJS (for Node) and ES module (for bundlers) build.
-	// // (We could have three entries in the configuration array
-	// // instead of two, but it's quicker to generate multiple
-	// // builds from a single configuration where possible, using
-	// // an array for the `output` option, where we can specify 
-	// // `file` and `format` for each target)
-	// {
-	// 	input: 'src/main.js',
-	// 	external: ['ms'],
-	// 	output: [
-	// 		{ file: pkg.main, format: 'cjs' },
-	// 		{ file: pkg.module, format: 'es' }
-	// 	]
-	// }
+  {
+    input: 'src/index',
+    output: {
+      file: './lib/async-fetch-client.js',
+      format: 'cjs',
+    },
+    plugins: [
+      resolve(),
+      babel({
+        babelrc: false,
+        presets: [
+          [
+            'env',
+            {
+              targets: {
+                browsers: ['ie 11', 'ios 7'],
+              },
+              modules: false,
+            },
+          ],
+          'flow',
+        ],
+      }),
+    ],
+  },
 ];
--- src/index.js
+++ src/index.js
@@ -1,15 +1,64 @@
-export const test = (x) => {
-  if (Array.isArray(x)) {
-    console.log('hellooo');
+// @flow
+import Cookies from 'js-cookie';
+
+type Headers = {
+  accept?: string,
+  authorization?: string,
+  'content-type'?: string,
+};
+type Method = 'GET' | 'DELETE' | 'PUT' | 'POST';
+type ModuleOptions = {
+  basePath: string,
+  body?: {},
+  headers?: Object,
+  includeCookies?: boolean,
+  method: Method,
+  url: string,
+};
+type RequestOptions = {
+  body?: string,
+  credentials?: 'include',
+  headers: Headers,
+  method: Method,
+};
+
+const BASE_PATH = '/api';
+const DEFAULT_HEADERS: Headers = {
+  accept: 'application/json',
+  'content-type': 'application/json',
+};
+
+const apiClient = async ({
+  basePath,
+  body,
+  headers,
+  includeCookies,
+  method,
+  url,
+}: ModuleOptions) => {
+  const fetchOptions: RequestOptions = {
+    headers: headers || DEFAULT_HEADERS,
+    method,
+  };
+
+  // If running on server, then don't bother fetching cookies.
+  const SID = window !== undefined && Cookies.get('SID');
+
+  if (includeCookies) fetchOptions.credentials = 'include';
+  if (typeof SID === 'string') {
+    fetchOptions.headers.authorization = `Bearer ${SID}`;
+  }
+  if (typeof body === 'object') fetchOptions.body = JSON.stringify(body);
+
+  const fetchUrl = `${basePath || BASE_PATH}${url}`;
+
+  try {
+    const response = await fetch(fetchUrl, fetchOptions);
+    const json = await response.json();
+    return { json };
+  } catch (error) {
+    return { error };
   }
 };
 
-export class What {
-  constructor(props) {
-    this.apples = props;
-  }
-
-  fuck() {
-    console.log('fuck');
-  }
-}
+export default apiClient;
